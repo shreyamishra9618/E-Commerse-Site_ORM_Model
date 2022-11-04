3fc6a27ec1424fea2a37d111390cb4d9902af40e
shreyamishra9618 <shreyamishra9618@gmail.com>
2022-11-03 22:14:49 -0700

    created the modal

--- models/Category.js
+++ models/Category.js
@@ -3,10 +3,21 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
-
+ // defined columns
 Category.init(
-  {
-    // define columns
+  { id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+    
+  },
+  category_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+
+  }
+   
   },
   {
     sequelize,
